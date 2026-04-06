e6402ea5c4c1892522fb51739222352cb1ffdd87
Conor Lynch-Jackson <conorgjackson@gmail.com>
2026-04-06 23:52:29 +0100

    Wrote Model class to provide interface for language model

--- src/generation.py
+++ src/generation.py
@@ -1,16 +1,25 @@
 import openai
 import base64
 
-with open("secrets/apikey.txt","r") as key_file:
-    apikey = key_file.read()
+class Model:
+    def __init__(self,keyfile="secrets/apikey.txt",preamble="Draw a 2d top down map, for a TTRPG, based on the following description, given by the game master to the players. Ignore and do not include any people, creatures or animals."):
 
-client = openai.OpenAI(api_key=apikey)
+        with open(keyfile,"r") as key_file:
+            apikey = key_file.read()
 
-response = client.responses.create(
-    model = "gpt-5-mini",
-    input = "Draw a 2d top down map based on the following description. Ignore and do not include any people, creatures or animals. You enter a tavern with a bar along the north wall, a door and windows on the west wall and 3 tables, with chairs.",
-    tools = [{"type" : "image_generation"}]
-)
+        self.preamble = preamble
+        self._client = openai.OpenAI(api_key=apikey)
+
+    def text_generation(self,prompt):
+        return self._client.responses.create(
+            model = "gpt-5-mini",
+            input =  self.preamble + prompt,
+            tools = [{"type" : "image_generation"}]
+        )
+
+gpt_model = Model()
+
+response = gpt_model.text_generation("You enter a tavern by a door on on the west side. At bar along the south wall where an orc barman is cleaning glasse. Patrons sit at tables scattered around the room. A hooded stranger sits alone at a table in the corner")
 
 print(list(response.output))
 
@@ -21,6 +30,6 @@ image_data = [
 ]
     
 if image_data:
-    image_base64 = image_data[0]
-    with open("outputs/tavern.png", "wb") as f:
-        f.write(base64.b64decode(image_base64))
+    for (i,img) in enumerate(image_data):
+        with open(f"outputs/tavern{i}.png", "wb") as f:
+            f.write(base64.b64decode(img))
