c7db42c0979d802de77eb6608406c7929e3f3630
Conor Lynch-Jackson <conorgjackson@gmail.com>
2026-04-07 22:57:07 +0100

    Have started writing command interface for bot and voice channel joining code

--- src/discord_bot.py
+++ src/discord_bot.py
@@ -6,19 +6,66 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+voiceclients = []
+
+def strip_mention(msg : str):
+    return "".join(msg.split(client.user.mention)).strip()
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+
+async def record(message : discord.Message):
+    print("record not implimented")
+
+async def stop_record(message : discord.Message):
+    print("stop_record not implimented")
+
+async def join(message : discord.Message):
+    if message.channel_mentions:
+        for channel in message.channel_mentions:
+            if isinstance(channel,discord.VoiceChannel):
+                new_voice_client = await channel.connect(self_mute=True)
+                voiceclients.append(new_voice_client)
+                return
+    content = strip_mention(message.content)
+
+    for channel in message.guild.voice_channels:
+        if content.startswith("join " + channel.name):
+            new_voice_client = await channel.connect(self_mute=True)
+            voiceclients.append(new_voice_client)
+            return
+
+    await message.channel.send("I am sorry. I cannot determine which voice channel you want me to join")
+
+
+
+commands = {
+    "listen" : record,
+    "stop" : stop_record,
+    "join" : join
+}
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    if not client.user.mentioned_in(message):
+        return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    content = strip_mention(message.content)
+
+    print(content)
+
+    for (command,action) in commands.items():
+        if content.startswith(command):
+            await action(message)
+            break
+
 
 with open("secrets/discord_token.txt","r") as token_file:
     token = token_file.read()
+
 
 client.run(token)
