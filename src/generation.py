21fdbb1e37f99c0d980b7a039115f8e7cb4143f7
Conor Lynch-Jackson <conorgjackson@gmail.com>
2026-04-07 15:24:41 +0100

    Added generate_from_voice. Moved test code into if __name__ ==" __main__"...

--- src/generation.py
+++ src/generation.py
@@ -1,6 +1,8 @@
 import openai
 import base64
 
+from parse_voice import parse_voice
+
 class Model:
     def __init__(self,keyfile="secrets/apikey.txt",preamble="Draw a 2d top down map, for a TTRPG, based on the following description, given by the game master to the players. Ignore and do not include any people, creatures or animals."):
 
@@ -10,26 +12,32 @@ class Model:
         self.preamble = preamble
         self._client = openai.OpenAI(api_key=apikey)
 
-    def text_generation(self,prompt):
+    def generate_from_text(self,prompt):
         return self._client.responses.create(
             model = "gpt-5-mini",
             input =  self.preamble + prompt,
             tools = [{"type" : "image_generation"}]
         )
 
-gpt_model = Model()
+    def generate_from_voice(self,wave_file):
+        return self.generate_from_text(parse_voice(wave_file))
 
-response = gpt_model.text_generation("You enter a tavern by a door on on the west side. At bar along the south wall where an orc barman is cleaning glasse. Patrons sit at tables scattered around the room. A hooded stranger sits alone at a table in the corner")
 
-print(list(response.output))
+if __name__ == "__main__":
 
-image_data = [
-    output.result
-    for output in response.output
-    if output.type == "image_generation_call"
-]
+    gpt_model = Model()
+
+    response = gpt_model.generate_from_text("You enter a tavern by a door on on the west side. At bar along the south wall where an orc barman is cleaning glasse. Patrons sit at tables scattered around the room. A hooded stranger sits alone at a table in the corner")
+
+    print(list(response.output))
+
+    image_data = [
+        output.result
+        for output in response.output
+        if output.type == "image_generation_call"
+    ]
     
-if image_data:
-    for (i,img) in enumerate(image_data):
-        with open(f"outputs/tavern{i}.png", "wb") as f:
-            f.write(base64.b64decode(img))
+    if image_data:
+        for (i,img) in enumerate(image_data):
+            with open(f"outputs/tavern{i}.png", "wb") as f:
+                f.write(base64.b64decode(img))
