a0683ef53f3937bc89ed174d5e212050e19ee857
Conor Lynch-Jackson <conorgjackson@gmail.com>
2026-04-07 17:22:17 +0100

    Set up simple discord bot that can log in and send messages

---
+++ src/discord_bot.py
@@ -0,0 +1,24 @@
+import discord
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+with open("secrets/discord_token.txt","r") as token_file:
+    token = token_file.read()
+
+client.run(token)
