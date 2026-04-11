a5be4a8e202098244fab773d98063dfd4265b217
Conor Lynch-Jackson <conorgjackson@gmail.com>
2026-04-11 22:28:41 +0100

    Added docstrings to CommandBot

--- src/command_bot.py
+++ src/command_bot.py
@@ -3,8 +3,14 @@ from functools import wraps, partial
 from discord.ext import commands
 
 class CommandBot():
+    """
+    A class which allows subclasses to register methods as commands with discord.py
+    """
     def __init__(self,*args,**kwargs):
         self.bot = commands.Bot(*args,**kwargs)
 
     def add_command_method(self,name,f):
+        """
+        Registers a method, f, of this class as a command with discord.py, called name
+        """
         self.bot.command(name=name)(wraps(f)(partial(f, self)))
